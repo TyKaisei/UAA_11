e3c9ca0ba8940cc4dbc1285d77e44ef30cecae1f
Dethier Mathéis <230437@CCM-Asty-Moulin.local>
2023-11-30 15:01:16 +0100

    Procédures / Fonctions

---
+++ 5TTI_MatheisD_ProcéduresFonctions/MesMéthodes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5TTI_MatheisD_ProcédureFonctions
+{
+    public struct MesMéthodes
+    {
+        public int NbRandom()
+        {
+            int alea;
+
+            Random random = new Random();
+            alea = random.Next(1, 7);
+
+            return alea;
+        }
+
+        public void LireReel(string question, out int n)
+        {
+
+            string nUser;
+
+            do
+            {
+                Console.WriteLine(question);
+                nUser = Console.ReadLine();
+            } while (!int.TryParse(nUser, out n));
+
+        }
+
+        public void TabRdm(int n, out int[] tab)
+        {
+
+            tab = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                tab[i] = NbRandom();
+            }
+        }
+
+        public void Concatenation(int[] tableauRandom, out string chaine)
+        {
+            chaine = "";
+
+            for (int i = 0; i < tableauRandom.Length; i++)
+            {
+                chaine = chaine + tableauRandom[i] + ", ";
+            }
+        }
+    }
+}
---
+++ 5TTI_MatheisD_ProcéduresFonctions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _5TTI_MatheisD_ProcédureFonctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string questions = "Combien de fois voulez-vous lancer votre dé ? ";
+            int n;
+            string chaine;
+            int[] tableauRandom;
+            MesMéthodes Outils = new MesMéthodes();
+            Outils.LireReel(questions, out n);
+            Outils.TabRdm(n, out tableauRandom);
+            Outils.Concatenation(tableauRandom, out chaine);
+            Console.WriteLine(chaine);
+        }
+    }
+}
